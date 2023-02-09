d859908742df050ef311848f4cd9ca06b7c9dd6e
Annetta Stogniew <stogniew.a@northeastern.edu>
2023-02-09 17:51:58 -0500

    functionality for recommendation

--- app.js
+++ app.js
@@ -3,6 +3,15 @@
 const questionDisplay = document.querySelector('#questions')
 const answerDisplay = document.querySelector('#answer')
 
+const recs = {
+    'pop': ['post-teen pop', 'neo mellow', 'indie poptimism', 'dream pop', 'alt-z'],
+    'country': ['country road', 'nl folk', 'mellow gold', 'redneck', 'red dirt'],
+    'dance': ['brostep', 'destroy techno', 'bubble trance', 'drift phonk', 're:techno'],
+    'indie': ['small room',  'lo-fi indie', 'shoegaze', 'neo-pagan', 'baroque pop'],
+    'rap': ['conscious hip hop', 'experimental hip hop', 'crunk', 'drain', 'dark trap'],
+    'rock': ['jangle pop', 'freakbeat', 'irish rebel song', 'anti-folk', 'souldies']
+}
+
 const questions = [
     {
        id: 0,
@@ -12,37 +21,43 @@ const questions = [
                text: "Taylor Swift",
                image: "https://i.scdn.co/image/ab6761610000e5eb5a00969a4698c3132a15fbb0",
                alt: "Singer Taylor Swift",
-               credit: "Spotify"
+               credit: "Spotify",
+               genre: "pop"
            },
            {
                text: "Kendrick Lamar",
                image: "https://i.scdn.co/image/ab6761610000e5eb437b9e2a82505b3d93ff1022",
                alt:"Singer Kendrick Lamar",
-               credit: "Spotify"
+               credit: "Spotify",
+               genre: "rap"
            },
            {
                text: "Rüfüs Du Sol",
                image: "https://i.scdn.co/image/ab6761610000e5eba5d83556275bb22511f57624",
                alt:"The band Rüfüs Du Sol",
-               credit: "Spotify"
+               credit: "Spotify",
+               genre: "edm"
            },
            {
                text: "Luke Combs",
                image: "https://i.scdn.co/image/ab6761610000e5eb3bf3adbf8f93255293523355",
                alt:"Singer Luke Combs",
-               credit: "Spotify"
+               credit: "Spotify",
+               genre: "country"
            },
            {
                 text: "Del Water Gap",
                 image: "https://i.scdn.co/image/ab6761610000e5eb8de628a55c6537a096e028e6",
                 alt:"Singer Del Water Gap",
-                credit: "Spotify"
+                credit: "Spotify",
+                genre: "indie"
             },
             {
                 text: "Fleetwood Mac",
                 image: "https://i.scdn.co/image/ab6761610000e5eb249d55f2d68a44637905c57e",
                 alt:"Gothic font in red reads, 'Fleetwood Mac' against white background",
-                credit: "Spotify"
+                credit: "Spotify",
+                genre: "rock"
             }
        ]
     },
@@ -54,37 +69,43 @@ const questions = [
                 text: "Electric Light Orchestra",
                 image: "https://i.scdn.co/image/ab6761610000e5eb3f289b61b4d965ce54e31841",
                 alt:"The band Electric Light Orchestra's signature image of a UFO",
-                credit: "Spotify"
+                credit: "Spotify",
+                genre: "rock"
             },
             {
                 text: "Dustin Lynch",
                 image: "https://i.scdn.co/image/ab6761610000e5eb81cd620ab4e2adb53d83c5f9",
                 alt:"Singer Dustin Lynch",
-                credit: "Spotify"
+                credit: "Spotify",
+                genre: "country"
             },
             {
                 text: "GRiZ",
                 image: "https://i.scdn.co/image/ab6761610000e5eb70fa7f3e6cb3df4c100fd43a",
                 alt:"Singer GRiZ",
-                credit: "Spotify"
+                credit: "Spotify",
+                genre: "edm"
             },
             {
                 text: "Earl Sweatshirt",
                 image: "https://i.scdn.co/image/ab6761610000e5eb5e93db92ca7864585fbe5f28",
                 alt:"Singer Earl Sweatshirt",
-                credit: "Spotify"
+                credit: "Spotify",
+                genre: "rap"
             },
             {
                 text: "Japanese Breakfast",
                 image: "https://i.scdn.co/image/ab6761610000e5eb3a3fed0c7aa2276bedccc7fe",
                 alt:"Singer Japanese Breakfast",
-                credit: "Spotify"
+                credit: "Spotify",
+                genre: "indie"
             },
             {
                 text: "I don't know any of these",
                 image: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/Emblem-question-yellow.svg/240px-Emblem-question-yellow.svg.png",
                 alt:"A white question mark inside of a yellow circle with a dropshadow",
-                credit: "Creative Commons"
+                credit: "Creative Commons",
+                genre: "pop"
             }
         ]
     },
@@ -96,37 +117,43 @@ const questions = [
                 text: "Ascend - Illenium",
                 image: "https://i.scdn.co/image/ab67616d0000b273529c6fa82d23f65076c1579b",
                 alt:"Computer-genrated image of a flaming angel against a black background",
-                credit: "Spotify"
+                credit: "Spotify",
+                genre: "edm"
             },
             {
                 text: "Come Over When You're Sober - Lil Peep",
                 image: "https://i.scdn.co/image/ab67616d0000b27322278aa84691381f3015d534",
                 alt:"Singer Lil Peep lying across a bed next to a pink sneaker and pink stuffed rabbit",
-                credit: "Spotify"
+                credit: "Spotify",
+                genre: "rap"
             },
             {
                 text: "SOUR - Olivia Rodrigo",
                 image: "https://i.scdn.co/image/ab67616d0000b273a91c10fe9472d9bd89802e5a",
                 alt:"Singer Olivia Rodrigo sticking her tongue out with stickers all over her face",
-                credit: "Spotify"
+                credit: "Spotify",
+                genre: "pop"
             },
             {
                 text: "The Smiths - The Smiths",
                 image: "https://i.scdn.co/image/ab67616d0000b273a09b231129ab1cb1a6efc57f",
                 alt:"Purple filter over a black and white torso-up shot of a shirtless man looking down",
-                credit: "Spotify"
+                credit: "Spotify",
+                genre: "rock"
             },
             {
                 text: "American Heartbreak - Zach Bryan",
                 image: "https://i.scdn.co/image/ab67616d0000b273ba30859a1ca79c5a372d177a",
                 alt:"A blurred man runs across a field at sunset",
-                credit: "Spotify"
+                credit: "Spotify",
+                genre: "country"
             },
             {
                 text: "Cigarettes After Sex - Cigarettes After Sex",
                 image: "https://i.scdn.co/image/ab67616d0000b2736b701428ed4c6e053902174a",
                 alt:"Cigarettes after sex written in all caps in white font against black background",
-                credit: "Spotify"
+                credit: "Spotify",
+                genre: "indie"
             }
         ]
     },
@@ -138,37 +165,43 @@ const questions = [
                 text: "~alternative~",
                 image: "https://cdn.theatlantic.com/thumbor/eiecJzSaSSwQQPK0ygvlLCB1YSM=/0x396:2450x2846/1080x1080/media/img/2015/05/BOB_Cohen/original.jpg",
                 alt:"Cartoon drawing of a crowd at a concert from the perspective of the stage",
-                credit: "The Atlantic"
+                credit: "The Atlantic",
+                genre: "indie"
             },
             {
                 text: "classic",
                 image: "https://images.unsplash.com/photo-1460317442991-0ec209397118?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
                 alt:"low angle view of building",
-                credit: "Brandon Giggs"
+                credit: "Brandon Giggs",
+                genre: "rock"
             },
             {
                 text: "headache-inducing",
                 image: "https://images.unsplash.com/photo-1572120360610-d971b9d7767c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
                 alt:"trees beside white house",
-                credit: "Phil Hearing"
+                credit: "Phil Hearing",
+                genre: "edm"
             },
             {
                 text: "basic",
                 image: "https://images.unsplash.com/photo-1506974210756-8e1b8985d348?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
                 alt:"brown wooden cabin infront of forest",
-                credit: "eulauretta"
+                credit: "eulauretta",
+                genre: "pop"
             },
             {
                 text: "valid",
                 image: "https://images.unsplash.com/photo-1572120360610-d971b9d7767c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
                 alt:"trees beside white house",
-                credit: "Phil Hearing"
+                credit: "Phil Hearing",
+                genre: "rap"
             },
             {
                 text: "bad",
                 image: "https://images.unsplash.com/photo-1506974210756-8e1b8985d348?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
                 alt:"brown wooden cabin infront of forest",
-                credit: "eulauretta"
+                credit: "eulauretta",
+                genre: "country"
             }
         ]
     },
@@ -180,37 +213,43 @@ const questions = [
                 text: "fila disruptors",
                 image: "https://cdn.theatlantic.com/thumbor/eiecJzSaSSwQQPK0ygvlLCB1YSM=/0x396:2450x2846/1080x1080/media/img/2015/05/BOB_Cohen/original.jpg",
                 alt:"Cartoon drawing of a crowd at a concert from the perspective of the stage",
-                credit: "The Atlantic"
+                credit: "The Atlantic",
+                genre: "edm"
             },
             {
                 text: "vejas",
                 image: "https://images.unsplash.com/photo-1460317442991-0ec209397118?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
                 alt:"low angle view of building",
-                credit: "Brandon Giggs"
+                credit: "Brandon Giggs",
+                genre: "pop"
             },
             {
                 text: "af1",
                 image: "https://images.unsplash.com/photo-1572120360610-d971b9d7767c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
                 alt:"trees beside white house",
-                credit: "Phil Hearing"
+                credit: "Phil Hearing",
+                genre: "rap"
             },
             {
                 text: "tevas",
                 image: "https://images.unsplash.com/photo-1506974210756-8e1b8985d348?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
                 alt:"brown wooden cabin infront of forest",
-                credit: "eulauretta"
+                credit: "eulauretta",
+                genre: "rock"
             },
             {
                 text: "doc martens",
                 image: "https://images.unsplash.com/photo-1572120360610-d971b9d7767c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
                 alt:"trees beside white house",
-                credit: "Phil Hearing"
+                credit: "Phil Hearing",
+                genre: "indie"
             },
             {
                 text: "cowboy boots",
                 image: "https://images.unsplash.com/photo-1506974210756-8e1b8985d348?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
                 alt:"brown wooden cabin infront of forest",
-                credit: "eulauretta"
+                credit: "eulauretta",
+                genre: "country"
             }
         ]
     },
@@ -222,37 +261,43 @@ const questions = [
                 text: "There's a snake in my boot!",
                 image: "https://cdn.theatlantic.com/thumbor/eiecJzSaSSwQQPK0ygvlLCB1YSM=/0x396:2450x2846/1080x1080/media/img/2015/05/BOB_Cohen/original.jpg",
                 alt:"Cartoon drawing of a crowd at a concert from the perspective of the stage",
-                credit: "The Atlantic"
+                credit: "The Atlantic",
+                genre: "country"
             },
             {
                 text: "Have you met my friend Molly?",
                 image: "https://images.unsplash.com/photo-1460317442991-0ec209397118?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
                 alt:"low angle view of building",
-                credit: "Brandon Giggs"
+                credit: "Brandon Giggs",
+                genre: "edm"
             },
             {
                 text: "Bro did you know Tame Impala is just one guy?",
                 image: "https://images.unsplash.com/photo-1572120360610-d971b9d7767c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
                 alt:"trees beside white house",
-                credit: "Phil Hearing"
+                credit: "Phil Hearing",
+                genre: "indie"
             },
             {
                 text: "I heard it on TikTok first.",
                 image: "https://images.unsplash.com/photo-1506974210756-8e1b8985d348?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
                 alt:"brown wooden cabin infront of forest",
-                credit: "eulauretta"
+                credit: "eulauretta",
+                genre: "pop"
             },
             {
                 text: "Are you old enough to remember landlines?",
                 image: "https://images.unsplash.com/photo-1572120360610-d971b9d7767c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
                 alt:"trees beside white house",
-                credit: "Phil Hearing"
+                credit: "Phil Hearing",
+                genre: "rock"
             },
             {
                 text: "No cap",
                 image: "https://images.unsplash.com/photo-1506974210756-8e1b8985d348?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
                 alt:"brown wooden cabin infront of forest",
-                credit: "eulauretta"
+                credit: "eulauretta",
+                genre: "rap"
             }
         ]
     },
@@ -264,37 +309,43 @@ const questions = [
                 text: "Mick Jagger and David Bowie",
                 image: "https://cdn.theatlantic.com/thumbor/eiecJzSaSSwQQPK0ygvlLCB1YSM=/0x396:2450x2846/1080x1080/media/img/2015/05/BOB_Cohen/original.jpg",
                 alt:"Cartoon drawing of a crowd at a concert from the perspective of the stage",
-                credit: "The Atlantic"
+                credit: "The Atlantic",
+                genre: "rock"
             },
             {
                 text: "Phoebe Bridgers, Lucy Dacus and Julien Baker",
                 image: "https://images.unsplash.com/photo-1460317442991-0ec209397118?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
                 alt:"low angle view of building",
-                credit: "Brandon Giggs"
+                credit: "Brandon Giggs",
+                genre: "indie"
             },
             {
                 text: "Dolly Parton and Kenny Rogers",
                 image: "https://images.unsplash.com/photo-1572120360610-d971b9d7767c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
                 alt:"trees beside white house",
-                credit: "Phil Hearing"
+                credit: "Phil Hearing",
+                genre: "country"
             },
             {
                 text: "Two random girls who just met at a festival",
                 image: "https://images.unsplash.com/photo-1506974210756-8e1b8985d348?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
                 alt:"brown wooden cabin infront of forest",
-                credit: "eulauretta"
+                credit: "eulauretta",
+                genre: "edm"
             },
             {
                 text: "Bladee and Yung Lean",
                 image: "https://images.unsplash.com/photo-1572120360610-d971b9d7767c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
                 alt:"trees beside white house",
-                credit: "Phil Hearing"
+                credit: "Phil Hearing",
+                genre: "rap"
             },
             {
                 text: "Doja Cat and Saweetie",
                 image: "https://images.unsplash.com/photo-1506974210756-8e1b8985d348?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&fit=crop&h=230&w=320&crop=edges",
                 alt:"brown wooden cabin infront of forest",
-                credit: "eulauretta"
+                credit: "eulauretta",
+                genre: "pop"
             }
         ]
     }
@@ -350,7 +401,7 @@ const populateQuestions = () => {
         question.answers.forEach(answer => {
             const answerBlock = document.createElement('div')
             answerBlock.classList.add('answer-block')
-            answerBlock.addEventListener('click', () => handleClick(question.id, answer.text))
+            answerBlock.addEventListener('click', () => handleClick(question.id, answer.genre))
             const answerImage = document.createElement('img')
             answerImage.setAttribute('src', answer.image)
             answerImage.setAttribute('alt', answer.alt)
@@ -362,7 +413,7 @@ const populateQuestions = () => {
             const imageLink = document.createElement('a')
             imageLink.setAttribute('href', answer.image)
             imageLink.textContent = answer.credit
-            const sourceLink = document.createElement('a')
+            // const sourceLink = document.createElement('a')
             // sourceLink.textContent = 'Unsplash'
             // sourceLink.setAttribute('src', 'https://www.unsplash.com')
             // answerInfo.append(imageLink, ' to ', sourceLink)
@@ -400,28 +451,43 @@ const handleClick = (questionId, chosenAnswer) => {
 }
 
 const showAnswer = () => {
-    let result
-    answers.forEach(answer => {
-        if (
-            chosenAnswers.includes(answer.combination[0]) +
-            chosenAnswers.includes(answer.combination[1]) +
-            chosenAnswers.includes(answer.combination[2])
-        ) {
-            result = answer
-            return
-        } else if (!result) {
-            //first answer object is default
-            result = answers[0]
-        }
+    let result;
+    
+    const answerCounts = {
+        'pop': 0,
+        'country': 0,
+        'dance': 0,
+        'indie': 0,
+        'rap': 0,
+        'rock': 0
+    };
+    
+    chosenAnswers.forEach(chosenAnswer => {
+        answerCounts[chosenAnswer] += 1;
     })
+
+    let maxKey, maxValue = 0;
+
+    for(const [key, value] of Object.entries(answerCounts)) {
+        if(value > maxValue) {
+            maxValue = value;
+            maxKey = key;
+        }
+    }
+
+    result = recs[maxKey][Math.floor(Math.random()*recs[maxKey].length)];
 
     const answerBlock = document.createElement('div')
     answerBlock.classList.add('result-block')
     const answerTitle = document.createElement('h3')
-    answerTitle.textContent = result.text
+    answerTitle.textContent = result
+    // answerTitle.textContent = result.text
+    console.log('answerTitle', answerTitle.textContent)
     const answerImage = document.createElement('img')
+    /*
     answerImage.setAttribute('src', result.image)
     answerImage.setAttribute('alt', result.alt)
+    */
 
     answerBlock.append(answerTitle, answerImage)
 
